390d085933e3c5d338033deb4d8329e322176d10
AaronBoat <2360679476@qq.com>
2025-11-24 23:19:45 +0800

    feat: 通过pj测试网站，使用kmeans

--- MySolution.h
+++ MySolution.h
@@ -17,27 +17,27 @@ class Solution
 {
 private:
     // Algorithm selection
-    bool use_ivf;            // true for IVF, false for HNSW
-    
+    bool use_ivf; // true for IVF, false for HNSW
+
     // HNSW parameters
-    int M;                    // number of connections per vertex
-    int ef_construction;      // size of dynamic candidate list during construction
-    int ef_search;           // size of dynamic candidate list during search
-    int max_level;           // maximum level
-    float ml;                // level multiplier
+    int M;               // number of connections per vertex
+    int ef_construction; // size of dynamic candidate list during construction
+    int ef_search;       // size of dynamic candidate list during search
+    int max_level;       // maximum level
+    float ml;            // level multiplier
 
     // IVF parameters
-    int nlist;               // number of clusters
-    int nprobe;              // number of clusters to search
-    vector<vector<float>> centroids;  // cluster centroids
-    vector<vector<int>> inverted_lists;  // inverted lists: cluster_id -> list of vector ids
+    int nlist;                          // number of clusters
+    int nprobe;                         // number of clusters to search
+    vector<vector<float>> centroids;    // cluster centroids
+    vector<vector<int>> inverted_lists; // inverted lists: cluster_id -> list of vector ids
 
     // Data storage
     int dimension;
     int num_vectors;
     vector<float> vectors;
     vector<int> entry_point;
-    
+
     // HNSW graph structure
     // graph[level][vertex_id] = list of neighbors
     vector<vector<vector<int>>> graph;
@@ -47,32 +47,32 @@ private:
     mt19937 rng;
 
     // Distance calculation
-    float distance(const float* a, const float* b, int dim) const;
-    
+    float distance(const float *a, const float *b, int dim) const;
+
     // HNSW methods
     int random_level();
-    vector<int> search_layer(const float* query, const vector<int>& entry_points, 
-                            int ef, int level) const;
-    void select_neighbors_heuristic(vector<int>& neighbors, int M_level);
-    void connect_neighbors(int vertex, int level, const vector<int>& neighbors);
+    vector<int> search_layer(const float *query, const vector<int> &entry_points,
+                             int ef, int level) const;
+    void select_neighbors_heuristic(vector<int> &neighbors, int M_level);
+    void connect_neighbors(int vertex, int level, const vector<int> &neighbors);
     void build_hnsw();
-    void search_hnsw(const vector<float>& query, int* res);
-    
+    void search_hnsw(const vector<float> &query, int *res);
+
     // IVF methods
     void build_ivf();
     void kmeans_clustering();
-    int assign_to_nearest_centroid(const float* vec) const;
-    void search_ivf(const float* query, int* res) const;
-    
+    int assign_to_nearest_centroid(const float *vec) const;
+    void search_ivf(const float *query, int *res) const;
+
 public:
     Solution();
     ~Solution();
-    
+
     // Set HNSW parameters (call before build)
     void set_parameters(int M_val, int ef_c, int ef_s);
-    
-    void build(int d, const vector<float>& base);
-    void search(const vector<float>& query, int* res);
+
+    void build(int d, const vector<float> &base);
+    void search(const vector<float> &query, int *res);
 };
 
 #endif // MY_SOLUTION_H
